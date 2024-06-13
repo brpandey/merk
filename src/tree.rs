495c7485e1239862e81b9850db30eafca64bf5bf
Bibek Pandey <bibekp@gmail.com>
2024-06-12 18:42:40 -0700

    Added tree set logic with scale. Added tree creation and set tests and hashcat and hex tests. Added helper tree, hex, and hashcat functions.  Added a tree test only api functions as well. Added num-bigint for scale logic.

--- src/tree.rs
+++ src/tree.rs
@@ -8,32 +8,31 @@ const ROOT_INDEX: usize = 0;
 pub struct MerkleTree {
     depth: usize,
     size: usize,
+    leaf_start_index: usize,
     store: HashMap<usize, SharedHashPair>, // map tree index item to shared hash pair
     master: HashMap<String, SharedHashPair>, // store a single master copy of Rc value
 }
 
 impl MerkleTree {
     pub fn new(depth: usize, initial_value: &str) -> Self {
-        if depth > MAX_DEPTH {
-            panic!("Specified depth must be less than 30");
+        if depth == 0 || depth > MAX_DEPTH {
+            panic!("Specified depth must be greater than 0 and less than 30");
         }
 
-        let tree_size = |depth: usize| usize::pow(2, depth as u32) - 1;
-        let size = tree_size(depth);
-
+        let size = Self::size(depth);
         let store = HashMap::with_capacity(size); // maps tree index out to original rc values store in master
         let master = HashMap::with_capacity(depth); // shared out rc originals
 
         let mut tree = Self {
             depth,
             size,
+            leaf_start_index: Self::size(depth-1),
             store,
             master,
         };
 
         tree.initialize(initial_value)
             .expect("Unable to create tree");
-        //        if tree.initialize(initial_value).is_err() { return Default::default()};
 
         tree
     }
@@ -44,9 +43,17 @@ impl MerkleTree {
             .map_or(String::new(), |v| v.borrow().hash_key.clone())
     }
 
+    pub fn num_leaves(&self) -> usize {
+        self.size - self.leaf_start_index
+    }
+
+    pub(crate) fn size(depth: usize) -> usize {
+        usize::pow(2, depth as u32) - 1
+    }
+
     /******************* Index API functions ********************/
 
-    pub fn index(&self, depth: usize, offset: usize) -> usize {
+    pub(crate) fn index(&self, depth: usize, offset: usize) -> usize {
         let index = usize::pow(2, depth as u32) - 1 + offset; // (2,0) => 2^depth -1 + offset => 2^2 - 1 + 0 => 3 index
 
         if index >= self.size {
@@ -57,7 +64,7 @@ impl MerkleTree {
         index
     }
 
-    pub fn parent_index(&self, index: usize) -> usize {
+    pub(crate) fn parent_index(&self, index: usize) -> usize {
         if index == 0 || index >= self.size {
             return ROOT_INDEX;
         }
@@ -69,7 +76,7 @@ impl MerkleTree {
         }
     }
 
-    pub fn left_child_index(&self, index: usize) -> usize {
+    pub(crate)  fn left_child_index(&self, index: usize) -> usize {
         let child_index = 2 * index + 1;
         if child_index >= self.size {
             eprintln!("Child index is too large, and does not exist in tree");
@@ -78,7 +85,7 @@ impl MerkleTree {
         child_index
     }
 
-    pub fn right_child_index(&self, index: usize) -> usize {
+    pub(crate) fn right_child_index(&self, index: usize) -> usize {
         let child_index = 2 * index + 2;
 
         if child_index >= self.size {
@@ -87,6 +94,22 @@ impl MerkleTree {
         }
         child_index
     }
+
+    pub fn set_with_scale(&mut self, leaf_index: usize, scale: usize, value: &str) {
+        if value.len() % 2 == 1 {
+            eprintln!("unable to set as value is malformed as len is of odd length");
+            return
+        }
+
+        if !self.valid_leaf_index(leaf_index) { return }
+
+        let v = Hex::trim_prefix(value);
+        let value = if scale == 1 { v.to_string() } else { Hex::scale(scale, v) };
+
+        self.set(leaf_index + self.leaf_start_index, value)
+    }
+
+    /******************* Private helper functions  ********************/
 
     fn initialize(&mut self, value: &str) -> Result<(), MerkError> {
         let mut acc_hash_str = Hex::trim_prefix(value).to_string();
@@ -99,7 +122,7 @@ impl MerkleTree {
         // Until reach root node - root level
 
         for d in (0..self.depth).rev() {
-            let shared_hash = HashPair::new_shared_(acc_hash_str.clone(), acc_bytes);
+            let shared_hash = HashPair::new_shared(acc_hash_str.clone(), acc_bytes);
 
             self.master.insert(acc_hash_str, shared_hash.clone()); // store local variable into master store
 
@@ -117,6 +140,76 @@ impl MerkleTree {
 
         Ok(())
     }
+
+
+    // recursive set function that sets specified tree node index with appropriate
+    // hash string value
+    fn set(&mut self, index: usize, value: String) {
+        use std::rc::Rc;
+
+        let new_hash = HashPair::new_shared_key(value).unwrap(); // value already been validated
+
+        // Prune shared hashmap for bloat if a shared_hash is no longer being referenced
+
+        if let Some(shared_hash) = self.store.get(&index) {
+
+            // if this is the last reference to this shared hash other than
+            // the original, remove it from the store, and remove it from shared
+
+            if Rc::strong_count(&shared_hash) == 2 {
+                let str_key = &shared_hash.borrow().hash_key.clone();
+
+                self.store.remove(&index); // strong count should be 1 after
+                self.master.remove(str_key);
+            }
+        }
+
+        // Note: don't store it into master
+        // Put in store only, as this is a one-off
+        self.store.insert(index, new_hash.clone());
+
+        let p_index = self.parent_index(index);
+
+        if index == p_index { return } // reached the top, nothing further to do, abort from recursion
+
+        // odd index is left child, even is right child
+        let sibling_index = if index % 2 == 0 { index - 1 } else { index + 1 };
+        let sibling_hash = self.store.get(&sibling_index).unwrap();
+
+        // compute new parent hash based on new hash and existing sibling hash
+        let p_hash = HashCat::concat(&new_hash.borrow().bytes, &sibling_hash.borrow().bytes);
+        self.set(p_index, Hex::encode(&p_hash))
+    }
+
+    fn valid_leaf_index(&self, leaf_index: usize) -> bool {
+        if leaf_index + self.leaf_start_index < self.size {
+            true
+        }
+        else {
+            eprintln!("leaf index is not valid");
+            false
+        }
+    }
+
+    /******************* Test-only API ********************/
+
+    #[cfg(test)]
+    pub(crate) fn contains_key(&self, key: &str) -> bool {
+        self.master.contains_key(key)
+    }
+
+    #[cfg(test)]
+    pub(crate) fn contains_key_at(&self, index: usize, key: &str) -> bool {
+        if let Some(shared_item) = self.store.get(&index) {
+            shared_item.borrow().hash_key == key
+        } else {
+            false
+        }
+    }
+
+    #[cfg(test)]
+    pub(crate) fn master_size(&self) -> usize { self.master.len() }
+
 }
 
 #[cfg(test)]
@@ -147,12 +240,39 @@ mod tests {
     }
 
     #[test]
-    pub fn tree_create_test() {
-        //        let mut tree = MerkleTree::new(5, "abcde");
+    // Check tree creation with invalid parameters that are ultimately "unrecoverable"
+    pub fn tree_create_test1() {
+        // suppress panic output
+        std::panic::set_hook(Box::new(|_| {})); 
 
-        //        let mut tree = MerkleTree::new(50, "abcdef");
+        // catch unrecoverable panic
+        let panic1 = std::panic::catch_unwind(|| {
+           MerkleTree::new(5, "abcde");
+        });
 
-        let tree = MerkleTree::new(
+        assert!(&panic1.is_err());
+
+        // catch unrecoverable panic
+        let panic2 = std::panic::catch_unwind(|| {
+            MerkleTree::new(50, "abcdef");
+        });
+
+        assert!(&panic2.is_err());
+
+        // catch unrecoverable panic
+        let panic3 = std::panic::catch_unwind(|| {
+            MerkleTree::new(0, "abcdef");
+        });
+
+        assert!(&panic3.is_err());
+    }
+
+    #[test]
+    // Check large tree creation
+    pub fn tree_create_test2() {
+        let tree;
+
+        tree = MerkleTree::new(
             20,
             "0xabababababababababababababababababababababababababababababababab",
         );
@@ -161,5 +281,80 @@ mod tests {
             tree.root(),
             "d4490f4d374ca8a44685fe9471c5b8dbe58cdffd13d30d9aba15dd29efb92930"
         );
+    }
+
+    #[test]
+    // Check tree creation and verify node hashes
+    pub fn tree_create_test3() {
+        let mut tree;
+
+        // construct tree of depths 1 and 2, and size 1 and 3
+        tree = MerkleTree::new(1, "0xab");
+        assert_eq!(tree.root(), "ab"); // single node so no hash concats!
+
+        tree = MerkleTree::new(2, "0xab"); // depth of 2 size 3
+
+        assert_eq!(tree.root(), "5fdf86416e09b2b1c0b91afe771d4e6ada2b0daed0066f8aed911109e33f3e34");
+
+        // check level hashes for a tree of depth 3
+
+        tree = MerkleTree::new(3, "0xabababababababababababababababababababababababababababababababab");
+
+        let level_hash_str = [
+            "abababababababababababababababababababababababababababababababab", // 0 - leaf
+            "699fc94ff1ec83f1abf531030e324003e7758298281645245f7c698425a5e0e7", // 1 - inner
+            "a2422433244a1da24b3c4db126dcc593666f98365403e6aaf07fae011c824f09"  // 2 - root
+        ];
+
+        // ensure our computed hashes at various node levels are found within tree
+        for i in 0..level_hash_str.len() {
+            assert!(tree.contains_key(level_hash_str[i]));
+        }
+
+        // ensure our computed hashes are correct for each tree level, from leaves, inner to root
+        for i in 3..7 {
+            assert!(tree.contains_key_at(i, level_hash_str[0]));
+        }
+
+        for i in 1..3 {
+            assert!(tree.contains_key_at(i, level_hash_str[1]));
+        }
+
+        assert!(tree.contains_key_at(0, level_hash_str[2])); // verify root value
+        assert_eq!(tree.root(), level_hash_str[2]); // same
+    }
+
+
+    #[test]
+    pub fn test_tree_set() {
+        let mut tree;
+
+        tree = tree_set_with_scale(2);
+        assert_eq!(tree.root(), "aa679f6812decf546048d3d610fe9b3567d8c4b6d2e4013360180617c10acccc");
+
+        tree = tree_set_with_scale(5);
+        assert_eq!(tree.root(), "8ecaaf19d68ed7d67ae175628dd6f1e73ae7a8b35d7ed394dfb8f54a7feadc0f");
+    }
+
+    // helper function to create and modify tree with set with scale function
+    pub fn tree_set_with_scale(depth: usize) -> MerkleTree {
+        let mut tree = MerkleTree::new(depth, "0x0000000000000000000000000000000000000000000000000000000000000000");
+
+        // the master size should be the depth of the tree because that's the number of unique values
+        // essentially 1 unique value per level
+        assert_eq!(tree.master_size(), depth);
+
+        for i in 0..tree.num_leaves() {
+            tree.set_with_scale(i, i, "1111111111111111111111111111111111111111111111111111111111111111");
+        }
+
+        // since all the leaves have changed, the intermediate node values change as a result
+        // this percolates up to the root
+        // the previous master values that were resident in the tree should be overwritten with new percolated values
+        // hence the master values table should be empty
+
+        assert_eq!(tree.master_size(), 0);
+
+        tree
     }
 }
--- src/types.rs
+++ src/types.rs
@@ -14,12 +14,19 @@ impl HashPair {
         Self { hash_key, bytes }
     }
 
-    pub fn new_shared_(hash_key: String, bytes: Vec<u8>) -> SharedHashPair {
+    pub fn new_shared_key(hash_key: String) -> Result<SharedHashPair, MerkError> {
+        let bytes: Vec<u8> = Hex::decode(&hash_key)?;
+        Ok( Self::new_shared(hash_key, bytes) )
+    }
+
+    pub fn new_shared(hash_key: String, bytes: Vec<u8>) -> SharedHashPair {
         Rc::new(RefCell::new(HashPair::new(hash_key, bytes)))
     }
+
 }
 
 const HEX_PREFIX: &str = "0x";
+const HEX_BASE: u32 = 16;
 
 // Unit struct to wrap hex encode and decode
 pub struct Hex;
@@ -29,13 +36,30 @@ impl Hex {
         hex::encode(data)
     }
 
-    pub fn decode<T: AsRef<[u8]>>(data: T) -> Result<Vec<u8>, MerkError> {
+    pub fn decode(value: &str) -> Result<Vec<u8>, MerkError> {
+        let data = Hex::trim_prefix(value);
         let bytes = hex::decode(data)?;
         Ok(bytes)
     }
 
     pub fn trim_prefix(text: &str) -> &str {
         text.trim_start_matches(HEX_PREFIX)
+    }
+
+    // Given a value in hex string form, give ability to multiply it by scaling factor
+    pub fn scale(scale: usize, value: &str) -> String {
+        use num_bigint::BigInt;
+        use num_traits::FromPrimitive;
+        use std::iter::repeat;
+
+        let v = Self::trim_prefix(value);
+
+        if scale == 0 { return repeat("0").take(v.len()).collect::<String>() }
+
+        let big_value_int = BigInt::parse_bytes(v.as_bytes(), HEX_BASE).unwrap();
+        let scaled_value = &big_value_int * BigInt::from_usize(scale).unwrap();
+
+        scaled_value.to_str_radix(HEX_BASE)
     }
 }
 
@@ -60,10 +84,73 @@ impl HashCat {
     }
 }
 
-// Simple aggregate error type
+// Simple aggregate error type to give calling code options
 
-#[derive(Error, Debug)]
+#[derive(Error, Debug, PartialEq)]
 pub enum MerkError {
     #[error("Unable to decode hex string into bytes vector -- {0}")]
     Hex(#[from] hex::FromHexError),
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+
+    #[test]
+    pub fn hex_encode_decode_test() {
+        use hex::FromHexError::OddLength;
+        let error = MerkError::Hex(OddLength);
+
+        // normal decode and encode
+        assert_eq!(vec![171], Hex::decode("0xab").unwrap());
+        assert_eq!(Hex::encode(vec![171]), "ab");
+
+        // erroneous decode
+        assert_eq!(Hex::decode("0xabc").unwrap_err(), error);
+        assert_eq!("Unable to decode hex string into bytes vector -- Odd number of digits", error.to_string())
+    }
+
+    #[test]
+    pub fn hash_single_concat_test() {
+        let bytes = [171]; // str is 0xab e.g 10*16 + 11
+
+        let out = HashCat::concat(&bytes, &bytes);
+
+        assert_eq!(Hex::encode(out), "5fdf86416e09b2b1c0b91afe771d4e6ada2b0daed0066f8aed911109e33f3e34");
+    }
+
+    #[test]
+    pub fn hash_multiple_concat_test() {
+        let depth = 20;
+        let mut bytes = Hex::decode("0xabababababababababababababababababababababababababababababababab").unwrap();
+        let final_hash = "d4490f4d374ca8a44685fe9471c5b8dbe58cdffd13d30d9aba15dd29efb92930";
+
+        let level_bytes = ["699fc94ff1ec83f1abf531030e324003e7758298281645245f7c698425a5e0e7",
+                           "a2422433244a1da24b3c4db126dcc593666f98365403e6aaf07fae011c824f09",
+                           "ec46a8dbc7fb0da5753b11f3ff04ee6b7a2a979b168025d40394a0ff4cf2df59",
+                           "34fac4b8781d0b811746ec45623606f43df1a8b9009f89c5564e68025a6fd604",
+                           "b8b1810f54c4048913090d78983712bd54cd4bae4e236be1f294122388abef6b",
+                           "4a011043594c8c029ec6141932c555b99c464ab75734027aeb968ed87fd5275c",
+                           "90029acbe3254c63bc9dd4a8f1e4b8e27b4445bb5e5a5897af9251ec744f6f68",
+                           "1489ad5e85ce2b6cbccfd2f25f8d63d115ff80199afbc4ec4f6fc2484bf8d690",
+                           "c795494aa662dd012c5de6c52f0ab28ee9135fe846074d62bb7807cf98742fd9",
+                           "0684c3868080b6ec1e59f146537540b3d630d6134eb3b518ce5344b8760a0cb2",
+                           "700516179f04e9e01ebdbe2987e6aeb88ad46edf5eea903076ef39327ba5ba8b",
+                           "d5adfaba9b3c5018f7d23cd40aec48be24ec8ea7e1f861033490ee35de54716e",
+                           "d7d9ecf26ace864c9c0555464d32d51e2768d34e4c7a635463052a05d91e6720",
+                           "44ad1490179db284f6fa21d8effbd1ba6a3028042b96be9b249f538de3f57a85",
+                           "0b792ae9ba3ff7c8fb8c9e4763269193fc18841d2a668c4033ec2d8ccc6d7f58",
+                           "7dc85b760de6c2191d52216d9ddcfdd116a456d80bc3a627783b4218b4c57ea7",
+                           "ea93465267c9baf2feec9de4f15555ac2504eed493900ac033bbd0a8bb34ca63",
+                           "f460eaf964fa3cd41296e60efdbf6dd7df549c780c3fb9432e1784a89df84302",
+                           final_hash];
+
+        // simulate the hash concat over a tree of depth
+        for i in 0..depth-1 {
+            bytes = HashCat::concat(&bytes, &bytes);
+            assert_eq!(Hex::encode(&bytes), level_bytes[i]);
+        }
+    }
 }
